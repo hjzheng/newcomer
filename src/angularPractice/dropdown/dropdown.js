1ed87d8021e3c4c11f477a5e9c96996cfed990df
hjzheng <zhenghaoju700@163.com>
2016-07-09 22:48:33 +0800

    STORY[新练习]: dropdown指令

--- src/angularPractice/dropdown/dropdown.js
+++ src/angularPractice/dropdown/dropdown.js
@@ -5,18 +5,36 @@
 	angular.module('app', []);
 
 	angular.module('app').controller('MainController', function($scope) {
+		$scope.toggle = function(open) {
+			console.log('open:', open);
+		};
 
+		$scope.orderLabel = '排序';
+		$scope.orderIcon = '';
+		$scope.orderSortList = [
+			{label: '价格 由低到高', sort: 'price', dir: 'asc'},
+			{label: '价格 由高到低', sort: 'price', dir: 'desc'},
+			{label: '服务 由低到高', sort: 'serve', dir: 'asc'},
+			{label: '服务 由高到低', sort: 'serve', dir: 'desc'}
+		];
+		$scope.orderClick = function(order, $index) {
+			$scope.orderLabel = order.label.split(' ')[0];
+			$scope.orderIcon = order.dir;
+			$scope.selectedIndex = $index;
+		};
 	});
 
-	angular.module('app').constant('myDropdownToggleClass', 'dropdown-menu-show');
-	angular.module('app').constant('myDropdownToggleEvent', 'click');
+	angular.module('app').constant('myDropdownConfig', {
+		toggleClass: 'dropdown-menu-show',
+		toggleEvent: 'click'
+	});
 
-	angular.module('app').directive('myDropdownToggle', function(myDropdownToggleEvent) {
+	angular.module('app').directive('myDropdownToggle', function(myDropdownConfig) {
 		return {
 			restrict: 'A',
 			require: '^^myDropdown',
 			link: function(scope, iElement, iAttrs, dropdownCtrl) {
-				iElement.bind(myDropdownToggleEvent, function(event) {
+				iElement.bind(myDropdownConfig.toggleEvent, function(event) {
 					dropdownCtrl.toggle(event);
 				});
 			}
@@ -33,10 +51,11 @@
 		};
 	});
 
-	angular.module('app').directive('myDropdown', function($document, myDropdownToggleClass, myDropdownToggleEvent) {
+	angular.module('app').directive('myDropdown', function($document, myDropdownConfig, $parse) {
 		return {
 			restrict: 'A',
 			controller: function($scope, $element, $attrs) {
+
 				var vm = this;
 
 				vm.setDropdownMenu = function(ele) {
@@ -44,17 +63,19 @@
 				};
 
 				vm.open = function() {
-					vm.dropdownEle.addClass(myDropdownToggleClass);
+					vm.dropdownEle.addClass(myDropdownConfig.toggleClass);
 					$document.bind('click', dropdownClose);
+					$attrs.onToggle ? $parse($attrs.onToggle)($scope, {open: true}) : angular.noop;
 				};
 
 				vm.close = function() {
-					vm.dropdownEle.removeClass(myDropdownToggleClass);
+					vm.dropdownEle.removeClass(myDropdownConfig.toggleClass);
 					$document.unbind('click', dropdownClose);
+					$attrs.onToggle ? $parse($attrs.onToggle)($scope, {open: false}) : angular.noop;
 				};
 
 				vm.toggle = function(event) {
-					if (vm.dropdownEle.hasClass(myDropdownToggleClass)) {
+					if (vm.dropdownEle.hasClass(myDropdownConfig.toggleClass)) {
 						vm.close();
 					} else {
 						vm.open();
@@ -63,7 +84,7 @@
 				};
 
 				function dropdownClose(event) {
-					if (!$element[0].contains(event.target)) {
+					if ($attrs.autoClose === 'true' || !$element[0].contains(event.target)) {
 						vm.close();
 					}
 				}
