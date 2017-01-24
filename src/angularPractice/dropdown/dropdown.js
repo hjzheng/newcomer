00cce96835981933c843424ce1db090a6e9a14b4
hjzheng <zhenghaoju700@163.com>
2017-01-24 10:50:04 +0800

    update dropdown

--- src/angularPractice/dropdown/dropdown.js
+++ src/angularPractice/dropdown/dropdown.js
@@ -2,65 +2,15 @@
  * Created by hjzheng on 16/6/16.
  */
 (function() {
-	angular.module('app', []);
 
-	angular.module('app').controller('MainController', function($scope) {
-		$scope.toggle = function(open) {
-			console.log('open:', open);
-		};
+	angular.module('dropdown-ui', [])
 
-		$scope.orderLabel = '排序';
-		$scope.orderIcon = '';
-		$scope.orderSortList = [
-			{label: '价格 由低到高', sort: 'price', dir: 'asc'},
-			{label: '价格 由高到低', sort: 'price', dir: 'desc'},
-			{label: '服务 由低到高', sort: 'serve', dir: 'asc'},
-			{label: '服务 由高到低', sort: 'serve', dir: 'desc'}
-		];
-		$scope.orderClick = function(order, $index) {
-			$scope.orderLabel = order.label.split(' ')[0];
-			$scope.orderIcon = order.dir;
-			$scope.selectedIndex = $index;
-		};
-
-		$scope.autoClose = false;
-
-		$scope.address = '地区:';
-
-		$scope.province = province;
-
-		$scope.showCity = function(p) {
-			$scope.proName = p.name;
-			$scope.cityName = '';
-			$scope.areaName = '';
-			$scope.city = city.filter(function(c) {
-				return c.ProID === p.ProID;
-			});
-		};
-
-		$scope.showArea = function(c) {
-			$scope.cityName = c.name;
-			$scope.areaName = '';
-			$scope.area = area.filter(function(a) {
-				return a.CityID === c.CityID;
-			});
-		};
-
-		$scope.check = function(a) {
-			$scope.areaName = a.DisName;
-			$scope.autoClose = true;
-			window.setTimeout(function() {
-				$scope.autoClose = false;
-			});
-		};
-	});
-
-	angular.module('app').constant('myDropdownConfig', {
+	angular.module('dropdown-ui').constant('myDropdownConfig', {
 		toggleClass: 'dropdown-menu-show',
 		toggleEvent: 'click'
 	});
 
-	angular.module('app').directive('myDropdownToggle', function(myDropdownConfig) {
+	angular.module('dropdown-ui').directive('myDropdownToggle', function(myDropdownConfig) {
 		return {
 			restrict: 'A',
 			require: '^^myDropdown',
@@ -72,7 +22,7 @@
 		};
 	});
 
-	angular.module('app').directive('myDropdownMenu', function() {
+	angular.module('dropdown-ui').directive('myDropdownMenu', function() {
 		return {
 			restrict: 'A',
 			require: '^^myDropdown',
@@ -82,7 +32,7 @@
 		};
 	});
 
-	angular.module('app').directive('myDropdown', function($document, myDropdownConfig, $parse) {
+	angular.module('dropdown-ui').directive('myDropdown', function($document, myDropdownConfig, $parse) {
 		return {
 			restrict: 'A',
 			controller: function($scope, $element, $attrs) {
