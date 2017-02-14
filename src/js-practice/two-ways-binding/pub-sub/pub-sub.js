a59b0b26822ac4426213462e0ec222632dc0315f
hjzheng <zhenghaoju700@163.com>
2017-02-15 00:42:26 +0800

    简单的双向绑定实现 pubsub

--- src/js-practice/two-ways-binding/pub-sub/pub-sub.js
+++ src/js-practice/two-ways-binding/pub-sub/pub-sub.js
@@ -1,109 +1,91 @@
-function DataBinder(object_id) {
+function DataBinder() {
+
 	// 创建一个简单地 pubsub 对象
 	// 没有做任何优化处理和错误处理
 	// 更完整的 PubSub(也就是 EventBus), 可以参考 https://github.com/hjzheng/angular-utils/blob/master/src/utils/EventBus.js
-
 	var PubSub = {
 
 		callbacks: {},
 
-		on: function(msg, callback) {
+		subscribe: function(msg, callback) {
 			this.callbacks[msg] = this.callbacks[msg] || [];
 			this.callbacks[msg].push(callback);
 		},
 
 		publish: function() {
 			var args = Array.prototype.slice.call(arguments);
-			var msg = args[0];
-			var rest = args.slice(1, args.length);
+			var msg = args.shift();
 			this.callbacks[msg] = this.callbacks[msg] || [];
 			for (var i = 0, len = this.callbacks[msg].length; i < len; i++) {
-				this.callbacks[msg][i].apply(this, rest);
+				this.callbacks[msg][i].apply(this, args);
 			}
 		}
 	};
 
-	var data_attr = 'data-bind-' + object_id;
-	var message = object_id + ':change';
+	// 数据模型
+	var Modal = {
+		modals: {},
+		update: function(modalName, value) {
+			this.modals[modalName] = value;
+			console.log('模型发生了变化, 通知视图');
+			PubSub.publish('modal-2-view', modalName, value);
+		},
+		get: function(modalName) {
+			return this.modals[modalName];
+		},
+		set: function(modalName, value) {
+			this.modals[modalName] = value;
+		}
+	};
+
+	var directive = 'data-bind';
 
 	var changeHandler = function(evt) {
 		var target = evt.target;
-		var prop_name = target.getAttribute(data_attr);
+		var modalName = target.getAttribute(directive);
 
-		if (prop_name && prop_name !== '') {
+		if (modalName && modalName !== '') {
 			console.log('视图发生了变化, 通知 modal');
-			PubSub.publish(message, prop_name, target.value, 'view');
+			PubSub.publish('ui-2-modal', modalName, target.value);
 		}
 	};
 
 	// 监听变化事件并代理到 PubSub
 	if (document.addEventListener) {
+		// document.addEventListener('keyup', changeHandler, false);
 		document.addEventListener('change', changeHandler, false);
 	}
 
-	// PubSub 将变化传播到所有绑定元素
-	PubSub.on(message, function(prop_name, new_val, type) {
-
-		if (type === 'view') {
-			return;
-		}
+	PubSub.subscribe('modal-2-view', function(modalName, newValue) {
 
 		console.log('由于模型发生了变化, 更新视图');
 
-		var elements = document.querySelectorAll('[' + data_attr + '="' + prop_name + '"]');
-		var	tag_name;
+		var	tagName;
+		var elements = document.querySelectorAll('[' + directive + '="' + modalName + '"]');
 
 		for (var i = 0, len = elements.length; i < len; i++) {
-			tag_name = elements[i].tagName.toLowerCase();
+			tagName = elements[i].tagName.toLowerCase();
 
-			if (tag_name === 'input' || tag_name === 'textarea' || tag_name === 'select') {
-				elements[i].value = new_val;
+			if (tagName === 'input' || tagName === 'textarea' || tagName === 'select') {
+				elements[i].value = newValue;
 			} else {
-				elements[i].innerHTML = new_val;
+				elements[i].innerHTML = newValue;
 			}
 		}
 	});
 
-	return PubSub;
-}
-
-function User(uid) {
-	var binder = new DataBinder(uid);
-
-	var user = {
-		attributes: {},
-
-		// 属性设置器使用数据绑定器 PubSub 来发布变化
-		// eslint-disable-next-line
-		set: function(attr_name, val, bool = true) { // eslint-disable-line
-			this.attributes[attr_name] = val;
-			if (bool) {
-				console.log('模型发生了变化, 通知视图');
-				binder.publish(uid + ':change', attr_name, val, 'model');
-			}
-		},
-
-		get: function(attr_name) {
-			return this.attributes[attr_name];
-		},
-
-		_binder: binder
-	};
-
-	binder.on(uid + ':change', function(attr_name, new_val, type) {
-		if (type === 'model') {
-			return;
-		}
+	PubSub.subscribe('ui-2-modal', function(modalName, newValue) {
 		console.log('由于视图发生了变化, 更新模型');
-		user.set(attr_name, new_val, false);
+		Modal.set(modalName, newValue);
 	});
 
-	return user;
+	return Modal;
 }
 
 window.onload = function() {
-	var user = User(123);
-	user.set('name', 'hjzheng');
+	window.user = DataBinder();
+	user.update('name', 'hjzheng');
+	user.update('test', 'test');
 };
 
 // use.set 方法中 会发布事件 123:change (模型,修改数据)
